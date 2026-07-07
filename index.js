a60d7dbcbb769adbac036456d7fb3d8eba82cc17
waynewonder1 <wayne.wonder@student.moringaschool.com>
2026-07-07 22:31:53 +0300

    done

--- index.js
+++ index.js
@@ -44,4 +44,24 @@ const bookStore = {
 }
 
 // Write your code here!
+const bookStoreTitle = document.getElementById("header");
+bookStoreTitle.textContent = bookStore.name;
+
+const bookList = document.getElementById("book-list")
+for (const book of bookStore.books) {
+const bookContainer = document.createElement("li");
+const bookTitle = document.createElement("h3");
+const bookAuthor = document.createElement("p");
+const bookImage = document.createElement("img");
+
+ bookTitle.textContent = book.title;
+ bookAuthor.textContent = book.author; 
+ bookImage.src = book.imageUrl; 
+
+ bookContainer.append(bookTitle);
+ bookContainer.append(bookAuthor);
+ bookContainer.append(bookImage); 
+
+ bookList.append(bookContainer); 
+}
 
